7140d047b6f71f642caa05af3a8821e11bb83246
pavel.nikitin <pavel.nikitin@bonial.com>
2024-12-04 12:03:44 +0100

    Added security

--- src/main/kotlin/com/db/fetchfacts/controller/AdminController.kt
+++ src/main/kotlin/com/db/fetchfacts/controller/AdminController.kt
@@ -2,15 +2,16 @@ package com.db.fetchfacts.controller
 
 import com.db.fetchfacts.dto.StatisticResponse
 import com.db.fetchfacts.service.interfaces.FactService
+import jakarta.annotation.security.RolesAllowed
 import jakarta.ws.rs.GET
 import jakarta.ws.rs.Path
-import jakarta.ws.rs.core.Response
 
 @Path("/admin/statistics")
 class AdminController(
         private val factService: FactService
 ) {
     @GET
+    @RolesAllowed("admin")
     fun getAccessStatistics():  List<StatisticResponse> {
         return factService.getStatistics()
     }
